2c5d2b6dc50065d0ff3813cd3cbf2fcf7acbfbc4
Flame-eth <theflamepen@gmail.com>
2023-12-05 19:50:24 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "VersaTalk",
+  description: "Chat with anyone, anywhere in a language of your choice",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>
+        Lets build a SAAS
+      </h1>
+    </main>
+  )
+}
