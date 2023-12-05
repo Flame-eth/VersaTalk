4e76fdf569c6e5aa8e157e6f6ea386e724d0425b
Flame-eth <theflamepen@gmail.com>
2023-12-05 23:12:35 +0100

    dark mode feature added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        </body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,26 @@
-import Link from "next/link"
-
+import Link from "next/link";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
 
 const Logo = () => {
   return (
-   <Link href="/">
-    Go Home
+    <Link href="/" prefetch={false} className="overflow-hidden">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items-center justify-center"
+        >
+          <Image
+            priority
+            src="/logo.png"
+            alt="VersaTalk Logo"
+            className="dark:filter dark:invert"
+            fill
+          />
+        </AspectRatio>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
