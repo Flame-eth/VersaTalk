5d8a427537cb944be2e64f9b88afa2b77e63fa94
Flame-eth <theflamepen@gmail.com>
2023-12-06 19:04:44 +0100

    next auth session

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import Logo from "./Logo";
 import DarkModeToggle from "./DarkModeToggle";
 import UserButton from "./UserButton";
+import { authOptions } from "@/auth";
+import { getServerSession } from "next-auth";
 
-function Header() {
+async function Header() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
   return (
     <div className=" top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto ">
