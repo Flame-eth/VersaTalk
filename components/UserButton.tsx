a50377fa64e0f1aba47dede2eadc8486df20d920
Flame-eth <theflamepen@gmail.com>
2023-12-06 20:48:35 +0100

    sign out added

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -10,7 +10,7 @@ import {
 import UserAvatar from "./UserAvatar";
 import { Session } from "next-auth";
 import { Button } from "./ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 function UserButton({ session }: { session: Session | null }) {
   if (!session)
@@ -30,10 +30,10 @@ function UserButton({ session }: { session: Session | null }) {
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
+        {/* <DropdownMenuItem>Profile</DropdownMenuItem>
         <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem> */}
+        <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
