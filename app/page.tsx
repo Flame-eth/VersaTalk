127bb609eff33139a36b29753a8596989473051e
Flame-eth <theflamepen@gmail.com>
2023-12-10 00:34:36 +0100

    lower clip path

--- app/page.tsx
+++ app/page.tsx
@@ -38,20 +38,44 @@ export default function Home() {
                 >
                   Get Started
                 </Link>
-                <Link href="/pricing" className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-300">
-                  View Pricing <span aria-hidden="true">
-                  <MoveRight />
+                <Link
+                  href="/pricing"
+                  className="text-sm font-semibold leading-6 flex gap-x-2 justify-center items-center text-gray-900 dark:text-gray-300"
+                >
+                  View Pricing{" "}
+                  <span aria-hidden="true">
+                    <MoveRight size={20} />
                   </span>
-                  </Link>
+                </Link>
               </div>
             </div>
 
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                <Image unoptimized src="./logo.png" alt='App Screenshot' width={2432} height={1442} className="rounded-md shadow-2xl ring-1 ring-gray-900/10" />
+                <Image
+                  unoptimized
+                  src="./logo.png"
+                  alt="App Screenshot"
+                  width={2432}
+                  height={1442}
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                />
               </div>
             </div>
           </div>
+        </div>
+
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
         </div>
       </div>
     </main>
