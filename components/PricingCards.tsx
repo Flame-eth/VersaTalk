1be09795a2ae15a4b5c90d7458c73f5ac1478e42
Flame-eth <theflamepen@gmail.com>
2023-12-19 22:42:50 +0100

    pricing card ui fix

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -13,44 +13,46 @@ function PricingCards({ redirect }: { redirect: boolean }) {
             className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
             key={tier.id}
           >
-            <h3
-              id={tier.id + tier.name}
-              className="text-base font-semibold leading-7 text-indigo-600"
-            >
-              {tier.name}
-            </h3>
-            <div className="mt-4 flex flex-col  items-baseline gap-x-2">
-              {tier.priceMonthly ? (
-                <>
-                  <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    {tier.priceMonthly}
-                  </span>
-                  <span className="text-base font-semibold leading-7 text-gray-600 ">
-                    /mo
-                  </span>
-                </>
-              ) : (
-                <span className="text-5xl font-bold tracking-tight text-gray-900">
-                  Free
-                </span>
-              )}
-              <p className="mt-6 text-base leading-7 text-gray-600">
-                {tier.description}
-              </p>
-              <ul
-                role="list"
-                className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
+            <div className="">
+              <h3
+                id={tier.id + tier.name}
+                className="text-base font-semibold leading-7 text-indigo-600"
               >
-                {tier.features.map((feature) => (
-                  <li key={feature} className="flex gap-x-3">
-                    <CheckIcon
-                      className="flex-none w-5 h-6 text-indigo-600"
-                      aria-hidden="true"
-                    />
-                    {feature}
-                  </li>
-                ))}
-              </ul>
+                {tier.name}
+              </h3>
+              <div className="mt-4 flex flex-col  items-baseline gap-x-2">
+                {tier.priceMonthly ? (
+                  <>
+                    <span className="text-5xl font-bold tracking-tight text-gray-900">
+                      {tier.priceMonthly}
+                      <span className="text-base font-semibold leading-7 text-gray-600 ">
+                        /mo
+                      </span>
+                    </span>
+                  </>
+                ) : (
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">
+                    Free
+                  </span>
+                )}
+                <p className="mt-6 text-base leading-7 text-gray-600">
+                  {tier.description}
+                </p>
+                <ul
+                  role="list"
+                  className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
+                >
+                  {tier.features.map((feature) => (
+                    <li key={feature} className="flex gap-x-3">
+                      <CheckIcon
+                        className="flex-none w-5 h-6 text-indigo-600"
+                        aria-hidden="true"
+                      />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
             {redirect ? (
               <Link
